efd5ebc990b2ad0f5a78dbcfa46ffbaa96d92d31
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-09 16:54:02 -0800

    Added appbar to act as a block for the welcoming message to be shown without issue.

--- app/src/main/java/com/example/mamajohnspizzaapp/WelcomeFragment.kt
+++ app/src/main/java/com/example/mamajohnspizzaapp/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 
 class WelcomeFragment : Fragment() {
